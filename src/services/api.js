c45e013c1893ee1e471b2cf3658d030842ee38d9
Jefferson guilherme <jeffe.leandro@gmail.com>
2025-11-09 18:40:00 -0300

    atualizações de rota do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // backend Express
+  baseURL: "https://fynger.onrender.com", // backend Express
   withCredentials: true, // <— ESSENCIAL
 });
 
